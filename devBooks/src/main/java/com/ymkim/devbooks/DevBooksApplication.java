5c5846a5698477065169e87216a6ba6a72384bb9
ymkim97 <rladudaud97@naver.com>
2023-07-14 03:38:33 +0900

    initial commit

---
+++ devBooks/src/main/java/com/ymkim/devbooks/DevBooksApplication.java
@@ -0,0 +1,13 @@
+package com.ymkim.devbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBooksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevBooksApplication.class, args);
+    }
+
+}
---
+++ devBooks/src/test/java/com/ymkim/devbooks/DevBooksApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ymkim.devbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DevBooksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
