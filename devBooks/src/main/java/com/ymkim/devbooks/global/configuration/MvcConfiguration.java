016627f91956546123029c9acbfa6f95f3e780ff
ymkim97 <rladudaud97@naver.com>
2023-07-21 22:06:40 +0900

    refactor: Configuration 패키지 이동

--- devBooks/src/main/java/com/ymkim/devbooks/global/configuration/MvcConfiguration.java
+++ devBooks/src/main/java/com/ymkim/devbooks/global/configuration/MvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.ymkim.devbooks.configuration;
+package com.ymkim.devbooks.global.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
