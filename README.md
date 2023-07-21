a9bf6306ed963fab7d7680a7530f1164893de324
ymkim97 <rladudaud97@naver.com>
2023-07-21 18:46:28 +0900

    도커 compose 파일 작성

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # DevBooks
 개인 프로젝트 - 도서 판매 관리 시스템
+
+실행시키기 위해서는 먼저 도커를 실행시켜야 합니다.
+
+### For Mac M1 ~
+
+docker 폴더 내에서 터미널로 명령어 `docker-compose up`를 통해서 애플리케이션에 필요한 MySql 서버와
+데이터베이스를 실행 할 수 있습니다.
+
+### For Windows
+
+`docker-compose up` 명령어 실행 전 docker-compose.yml을 편집합니다. 
+
+기존 `platform: linux/arm64/v8`에서 `platform: linux/x86_64`로 바꾼 후 `docker-compose up`를 실행합니다. 
---
+++ docker/mysql-init.d/schema.sql
@@ -0,0 +1,38 @@
+CREATE TABLE customer
+(
+    customer_id BIGINT auto_increment PRIMARY KEY,
+    name        VARCHAR(30) NOT NULL,
+    phone       VARCHAR(20) NOT NULL,
+    address     VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE book
+(
+    book_id      BIGINT auto_increment PRIMARY KEY,
+    title        VARCHAR(50) NOT NULL UNIQUE,
+    author       VARCHAR(50) NOT NULL,
+    category     VARCHAR(30) NOT NULL,
+    price        BIGINT      NOT NULL,
+    published_at DATE        NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id     BIGINT auto_increment PRIMARY KEY,
+    customer_id  VARCHAR(50) DEFAULT NULL,
+    address      VARCHAR(100) NOT NULL,
+    postcode     VARCHAR(10)  NOT NULL,
+    created_at   datetime(6)  NOT NULL,
+    order_status VARCHAR(30)  NOT NULL,
+    CONSTRAINT fk_orders_to_customer FOREIGN KEY (customer_id) references customer (customer_id) ON DELETE CASCADE
+);
+
+CREATE TABLE order_item
+(
+    order_item_id BIGINT auto_increment PRIMARY KEY,
+    order_id      BIGINT  DEFAULT NULL,
+    book_id       BIGINT  NOT NULL,
+    quantity      INTEGER NOT NULL,
+    CONSTRAINT fk_order_item_to_orders FOREIGN KEY (order_id) references orders (order_id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_item_to_book FOREIGN KEY (book_id) references book (book_id) ON DELETE CASCADE
+);
